08831e140f820eb63d56aea18c1a5382ec1ab024
Eddu <azabacheeduardo85@gmail.com>
2026-04-11 10:28:17 -0500

    chore(auth): remove supertokens module and related imports from AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,25 +1,3 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { SuperTokensModule } from 'supertokens-nestjs';
-import UserRoles from 'supertokens-node/recipe/userroles';
-import { buildEmailPasswordRecipe } from './recipes/email-password.recipe';
-import { buildSessionRecipe } from './recipes/session.recipe';
-import UserMetadata from 'supertokens-node/recipe/usermetadata';
-
-@Module({
-  imports: [
-    SuperTokensModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        ...configService.get('supertokens'),
-        recipeList: [
-          buildEmailPasswordRecipe({ config: configService }),
-          buildSessionRecipe({ config: configService }),
-          UserRoles.init(),
-          UserMetadata.init(),
-        ],
-      }),
-    }),
-  ],
-})
+@Module({})
 export class AuthModule {}
