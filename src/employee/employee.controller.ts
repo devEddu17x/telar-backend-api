517743c2b33d61c929247829fd0fd61cae50f304
Eddu <azabacheeduardo85@gmail.com>
2026-04-11 10:41:26 -0500

    chore(employee): remove SuperTokens authentication logic from EmployeeController

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -2,53 +2,28 @@ import {
   Body,
   Controller,
   Get,
+  NotImplementedException,
   Patch,
-  UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { UpdateEmployeeDTO } from './dtos/update-employee.dto';
-import {
-  Session,
-  SuperTokensAuthGuard,
-  VerifySession,
-} from 'supertokens-nestjs';
-import { SessionContainer } from 'supertokens-node/recipe/session';
 import { EmployeeEntity } from './entities/employee.entity';
-@UseGuards(SuperTokensAuthGuard)
+@UseGuards()
 @Controller('employees')
 export class EmployeeController {
-  constructor(private readonly employeeService: EmployeeService) {}
+  constructor(private readonly employeeService: EmployeeService) { }
 
   @Get('me')
-  @VerifySession()
   async getEmployee(
-    @Session() session: SessionContainer,
   ): Promise<EmployeeEntity & { roles: string[] }> {
-    const appUserId = session.getAccessTokenPayload().appUserId;
-    if (!appUserId) {
-      throw new UnauthorizedException('Session missing app user id');
-    }
-    const [employee, userRoles] = await Promise.all([
-      this.employeeService.getEmployee(appUserId.v),
-      this.employeeService.getRolesForEmployee(session.getUserId()),
-    ]);
-    return { ...employee, roles: userRoles };
+    throw new NotImplementedException('Not implemented yet');
   }
 
   @Patch()
-  @VerifySession()
   async updateEmployee(
     @Body() updateEmployeeDTO: UpdateEmployeeDTO,
-    @Session() session: SessionContainer,
   ): Promise<EmployeeEntity> {
-    const appUserId = session.getAccessTokenPayload().appUserId;
-    if (!appUserId) {
-      throw new UnauthorizedException('Session missing app user id');
-    }
-    return await this.employeeService.updateEmployee(
-      appUserId.v,
-      updateEmployeeDTO,
-    );
+    throw new NotImplementedException('Not implemented yet');
   }
 }
