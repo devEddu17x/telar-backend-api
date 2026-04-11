f27a8d1804869a9f79108b6dd4c03f411b87769a
Eddu <azabacheeduardo85@gmail.com>
2026-04-11 10:38:48 -0500

    chore(auth): remove SuperTokens integration from CustomerController

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -8,60 +8,37 @@ import {
   Post,
   Query,
   UseGuards,
-  HttpCode,
-  HttpStatus,
 } from '@nestjs/common';
 import { CustomerService } from './services/customer-internal.service';
 import { CreateCustomerDTO } from './dtos/create-customer.dto';
-import { SuperTokensAuthGuard } from 'supertokens-nestjs';
 import { ROLES } from 'src/auth/constants/roles';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { CustomerEntity } from './entities/customer.entity';
 import { UpdateCustomerDTO } from './dtos/update-customer.dto';
-import { RegisterEcommerceUserDTO } from './dtos/register-ecommerce-user.dto';
-import { CustomerEcommerceService } from './services/customer-ecommerce.service';
 
 @Controller('customers')
 export class CustomerController {
   constructor(
     private readonly customerService: CustomerService,
-    private readonly customerEcommerceService: CustomerEcommerceService,
-  ) {}
-
-  @Post('register')
-  @HttpCode(HttpStatus.CREATED)
-  async registerEcommerceUser(@Body() registerDTO: RegisterEcommerceUserDTO) {
-    const customer =
-      await this.customerEcommerceService.registerEcommerceUser(registerDTO);
-
-    return {
-      message: 'Usuario registrado exitosamente',
-      customer: {
-        id: customer.id,
-        names: customer.names,
-        lastNames: customer.lastNames,
-        email: customer.email,
-      },
-    };
-  }
+  ) { }
 
   @Roles(ROLES.SELLER)
-  @UseGuards(SuperTokensAuthGuard, RolesGuard)
+  @UseGuards(RolesGuard)
   @Post()
   async createCustomer(@Body() customerDTO: CreateCustomerDTO) {
     return await this.customerService.createCustomer(customerDTO);
   }
 
   @Roles(ROLES.SELLER)
-  @UseGuards(SuperTokensAuthGuard, RolesGuard)
+  @UseGuards(RolesGuard)
   @Get()
   async getAllCustomers(): Promise<CustomerEntity[]> {
     return await this.customerService.getAllCustomers();
   }
 
   @Roles(ROLES.SELLER)
-  @UseGuards(SuperTokensAuthGuard, RolesGuard)
+  @UseGuards(RolesGuard)
   @Get('search')
   async searchCustomers(
     @Query('names') names?: string,
@@ -72,7 +49,7 @@ export class CustomerController {
   }
 
   @Roles(ROLES.SELLER)
-  @UseGuards(SuperTokensAuthGuard, RolesGuard)
+  @UseGuards(RolesGuard)
   @Patch(':id')
   async updateCustomer(
     @Body() customerDTO: UpdateCustomerDTO,
