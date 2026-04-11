a6b3bb179e8506ea3c6897dfb11ee13c7fe69664
Eddu <azabacheeduardo85@gmail.com>
2026-04-11 10:38:31 -0500

    chore(auth): remove CustomerEcommerceService from CustomerModule

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -3,12 +3,11 @@ import { CustomerService } from './services/customer-internal.service';
 import { CustomerController } from './customer.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomerEntity } from './entities/customer.entity';
-import { CustomerEcommerceService } from './services/customer-ecommerce.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CustomerEntity])],
-  providers: [CustomerService, CustomerEcommerceService],
+  providers: [CustomerService],
   controllers: [CustomerController],
-  exports: [CustomerService, CustomerEcommerceService],
+  exports: [CustomerService],
 })
-export class CustomerModule {}
+export class CustomerModule { }
