1bc65965d26614c45d8fc1e2ea86f03fc17c42c7
Eddu <azabacheeduardo85@gmail.com>
2026-04-11 08:10:23 -0500

    chore(main): remove supertokens in entry point

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SuperTokensExceptionFilter } from 'supertokens-nestjs';
-import supertokens from 'supertokens-node';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
 
@@ -13,12 +11,6 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   app.setGlobalPrefix(configService.get('api').prefix);
-  app.enableCors({
-    origin: configService.get('supertokens').appInfo.websiteDomain,
-    allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders()],
-    credentials: true,
-  });
-  app.useGlobalFilters(new SuperTokensExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
