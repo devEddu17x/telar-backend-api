2603ddc2d0c3290e3dac12c17712aa135ec58bae
Eddu <azabacheeduardo85@gmail.com>
2026-04-11 13:45:39 -0500

    refactor(roles): update ROLES enum and comment out unused ACTION and RESOURCE enums

--- src/auth/constants/roles.ts
+++ src/auth/constants/roles.ts
@@ -1,28 +1,28 @@
-enum ACTION {
-  CREATE = 'create',
-  READ = 'read',
-  UPDATE = 'update',
-  DELETE = 'delete',
-}
-
-enum RESOURCE {
-  CLOTHES = 'clothes',
-  EMPLOYEES = 'employees',
-  SELF = 'self',
-}
-
 export enum ROLES {
-  CUSTOMER = 'customer',
+  OWNER = 'owner',
   ADMIN = 'admin',
   SELLER = 'seller',
 }
 
-// format: ACTION:RESOURCE
-export const ROLE_PERMISSIONS: Record<string, string[]> = {
-  [ROLES.CUSTOMER]: [`${ACTION.READ}:${RESOURCE.CLOTHES}`],
-  [ROLES.ADMIN]: [
-    `${ACTION.CREATE}:${RESOURCE.CLOTHES}`,
-    `${ACTION.READ}:${RESOURCE.CLOTHES}`,
-  ],
-  [ROLES.SELLER]: [`${ACTION.READ}:${RESOURCE.CLOTHES}`],
-};
+// enum ACTION {
+//   CREATE = 'create',
+//   READ = 'read',
+//   UPDATE = 'update',
+//   DELETE = 'delete',
+// }
+
+// enum RESOURCE {
+//   CLOTHES = 'clothes',
+//   EMPLOYEES = 'employees',
+//   SELF = 'self',
+// }
+
+// // format: ACTION:RESOURCE
+// export const ROLE_PERMISSIONS: Record<string, string[]> = {
+//   [ROLES.OWNER]: [`${ACTION.READ}:${RESOURCE.CLOTHES}`],
+//   [ROLES.ADMIN]: [
+//     `${ACTION.CREATE}:${RESOURCE.CLOTHES}`,
+//     `${ACTION.READ}:${RESOURCE.CLOTHES}`,
+//   ],
+//   [ROLES.SELLER]: [`${ACTION.READ}:${RESOURCE.CLOTHES}`],
+// };
