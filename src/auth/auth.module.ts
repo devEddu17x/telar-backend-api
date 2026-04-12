dcc80d13f9c7b017c933c19131fdd3264f3db82e
Eddu <azabacheeduardo85@gmail.com>
2026-04-11 19:14:00 -0500

    feat(auth): initialize AuthModule with AuthService, AuthController, and CognitoService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,11 @@
 import { Module } from '@nestjs/common';
-@Module({})
+import { AuthService } from './services/auth.service';
+import { EmployeeModule } from 'src/employee/employee.module';
+import { AuthController } from './auth.controller';
+import { CognitoService } from './services/cognito.service';
+@Module({
+  imports: [EmployeeModule],
+  controllers: [AuthController],
+  providers: [AuthService, CognitoService],
+})
 export class AuthModule {}
