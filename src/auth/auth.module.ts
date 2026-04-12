61e80ca202645696e7d401460e9b2570676a6126
Eddu <azabacheeduardo85@gmail.com>
2026-04-12 16:14:06 -0500

    feat(auth): enhance authentication module with JWT strategy and guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './services/auth.service';
 import { EmployeeModule } from 'src/employee/employee.module';
 import { AuthController } from './auth.controller';
 import { CognitoService } from './services/cognito.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
 @Module({
-  imports: [EmployeeModule],
+  imports: [
+    EmployeeModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, CognitoService],
+  providers: [AuthService, CognitoService, JwtStrategy],
+  exports: [CognitoService, JwtStrategy, PassportModule],
 })
 export class AuthModule {}
