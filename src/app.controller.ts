821debc4780a1e1d48ae75120ca4e86b1671878a
eddu <azabacheeduardo85@gmail.com>
2026-05-13 07:00:22 -0500

    feat: add Swagger documentation for health check API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiDocHealth } from './app.doc';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('health')
+  @ApiDocHealth()
   healthCheck(): string {
     return this.appService.healthCheck();
   }
