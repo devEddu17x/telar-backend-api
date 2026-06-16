c65a36e0d9194a0d4737b1239e26241efd7066f8
eddu <azabacheeduardo85@gmail.com>
2026-06-16 07:34:09 -0500

    fix: remove unused action and resource enums from roles.ts

--- src/auth/constants/roles.ts
+++ src/auth/constants/roles.ts
@@ -8,26 +8,3 @@ export enum CREATABLE_ROLES {
   ADMIN = 'admin',
   SELLER = 'seller',
 }
-
-// enum ACTION {
-//   CREATE = 'create',
-//   READ = 'read',
-//   UPDATE = 'update',
-//   DELETE = 'delete',
-// }
-
-// enum RESOURCE {
-//   CLOTHES = 'clothes',
-//   EMPLOYEES = 'employees',
-//   SELF = 'self',
-// }
-
-// // format: ACTION:RESOURCE
-// export const ROLE_PERMISSIONS: Record<string, string[]> = {
-//   [ROLES.OWNER]: [`${ACTION.READ}:${RESOURCE.CLOTHES}`],
-//   [ROLES.ADMIN]: [
-//     `${ACTION.CREATE}:${RESOURCE.CLOTHES}`,
-//     `${ACTION.READ}:${RESOURCE.CLOTHES}`,
-//   ],
-//   [ROLES.SELLER]: [`${ACTION.READ}:${RESOURCE.CLOTHES}`],
-// };
