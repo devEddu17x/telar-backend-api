8416669c403515b6d0d451bdd9eae77af18bcde9
eddu <azabacheeduardo85@gmail.com>
2026-06-16 07:26:48 -0500

    fix: remove unused email configuration export

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -2,5 +2,4 @@ export { default as typeormConfig } from './typeorm.config';
 export { default as apiConfig } from './api.config';
 export { default as s3Config } from './s3.config';
 export { default as pinoLoggerConfig } from './pino-logger.config';
-export { default as emailConfig } from './mail.config';
 export { default as cognitoConfig } from './cognito.config';
