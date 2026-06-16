584deca61d5d719366c15affb5b3acee2a27f97f
eddu <azabacheeduardo85@gmail.com>
2026-06-16 08:11:30 -0500

    fix: update module import paths to reflect new directory structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './core/config/config.module';
-import { CustomerModule } from './customer/customer.module';
-import { EmployeeModule } from './employee/employee.module';
-import { ClothesModule } from './clothes/clothes.module';
-import { AdminModule } from './admin/admin.module';
-import { StorageModule } from './storage/storage.module';
-import { QuoteModule } from './quote/quote.module';
-import { OrderModule } from './order/order.module';
-import { AuthModule } from './auth/auth.module';
-import { TenantModule } from './tenant/tenant.module';
+import { CustomerModule } from './modules/customer/customer.module';
+import { EmployeeModule } from './modules/employee/employee.module';
+import { ClothesModule } from './modules/clothes/clothes.module';
+import { AdminModule } from './modules/admin/admin.module';
+import { StorageModule } from './modules/storage/storage.module';
+import { QuoteModule } from './modules/quote/quote.module';
+import { OrderModule } from './modules/order/order.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { TenantModule } from './modules/tenant/tenant.module';
 @Module({
   imports: [
     AuthModule,
